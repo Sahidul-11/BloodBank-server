58185f47efcc8a02986dfe9b936b50d2cc77d0ca
sahidul-11 <mdsahidulislam161113@gmail.com>
2024-06-03 18:45:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,131 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId, Db } = require('mongodb')
+const jwt = require('jsonwebtoken')
+
+const port = process.env.PORT || 8000;
+
+// middleware
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
+app.use(express.json())
+app.use(cookieParser())
+
+// Verify Token Middleware
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
+  console.log(token)
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.90yxez6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+async function run() {
+  try {
+    //db collection 
+    const divisionsCollection =client.db("BloodBank").collection("Division")
+    const districtCollection =client.db("BloodBank").collection("district")
+    const upazilaCollection =client.db("BloodBank").collection("upazilla")
+  
+    const userCollection =client.db("BloodBank").collection("user")
+  
+    // auth related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+    // Logout
+    app.get('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+        console.log('Logout successful')
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
+
+     //District ,thana
+     app.get("/division", async(req ,res)=>{
+      const result = await divisionsCollection.find().toArray()
+      res.send(result)
+
+     })
+     app.get("/district/:id", async(req ,res)=>{
+      const id = req.params.id;
+      const query ={division_id : id}
+      const result = await districtCollection.find(query).toArray()
+      res.send(result)
+
+     })
+     app.get("/upazila/:id", async(req ,res)=>{
+      const id = req.params.id;
+      const query ={district_id : id}
+      const result = await upazilaCollection.find(query).toArray()
+      res.send(result)
+
+     })
+  //  create user
+  app.post("/user", async (req ,res)=>{
+    const user =req.body;
+    const result =await userCollection.insertOne(user)
+    res.send(result)
+  })
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Hello from StayVista Server..')
+})
+
+app.listen(port, () => {
+  console.log(`StayVista is running on port ${port}`)
+})
